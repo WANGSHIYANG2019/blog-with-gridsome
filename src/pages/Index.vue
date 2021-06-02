8ed91dcddf1af32f20945c5ca482f24471905461
王诗阳 <15998632784@163.com>
2021-06-02 15:26:09 +0800

    committed on 20210519 afternoon

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -19,75 +19,105 @@
         <div class="container">
             <div class="row">
                 <div class="col-lg-8 col-md-10 mx-auto">
-                    <div class="post-preview">
+                    <div class="post-preview" v-for="edge in $page.posts.edges" :key="edge.node.id">
                         <a href="post.html">
                             <h2 class="post-title">
-                                Man must explore, and this is exploration at its greatest
+                                {{ edge.node.title }}
                             </h2>
                             <h3 class="post-subtitle">
-                                Problems look mighty small from 150 miles up
+                                Insert your own subtitle
                             </h3>
                         </a>
                         <p class="post-meta">Posted by
-                            <a href="#">Start Bootstrap</a>
+                            <a href="#">{{ edge.node.createdBy?edge.node.createdBy.username:'Not set yet' }}</a>
                             on September 24, 2019</p>
+                        <a href="" v-for="tag in edge.node.tags" :key="tag.id">{{tag.title}}</a>
+                        <hr>
                     </div>
-                    <hr>
-                    <div class="post-preview">
-                        <a href="post.html">
-                            <h2 class="post-title">
-                                I believe every human has a finite number of heartbeats. I don't intend to waste any of mine.
-                            </h2>
-                        </a>
-                        <p class="post-meta">Posted by
-                            <a href="#">Start Bootstrap</a>
-                            on September 18, 2019</p>
-                    </div>
-                    <hr>
-                    <div class="post-preview">
-                        <a href="post.html">
-                            <h2 class="post-title">
-                                Science has not yet mastered prophecy
-                            </h2>
-                            <h3 class="post-subtitle">
-                                We predict too much for the next year and yet far too little for the next ten.
-                            </h3>
-                        </a>
-                        <p class="post-meta">Posted by
-                            <a href="#">Start Bootstrap</a>
-                            on August 24, 2019</p>
-                    </div>
-                    <hr>
-                    <div class="post-preview">
-                        <a href="post.html">
-                            <h2 class="post-title">
-                                Failure is not an option
-                            </h2>
-                            <h3 class="post-subtitle">
-                                Many say exploration is part of our destiny, but it’s actually our duty to future generations.
-                            </h3>
-                        </a>
-                        <p class="post-meta">Posted by
-                            <a href="#">Start Bootstrap</a>
-                            on July 8, 2019</p>
-                    </div>
-                    <hr>
+<!--                    <div class="post-preview">-->
+<!--                        <a href="post.html">-->
+<!--                            <h2 class="post-title">-->
+<!--                                I believe every human has a finite number of heartbeats. I don't intend to waste any of mine.-->
+<!--                            </h2>-->
+<!--                        </a>-->
+<!--                        <p class="post-meta">Posted by-->
+<!--                            <a href="#">Start Bootstrap</a>-->
+<!--                            on September 18, 2019</p>-->
+<!--                    </div>-->
+<!--                    <hr>-->
+<!--                    <div class="post-preview">-->
+<!--                        <a href="post.html">-->
+<!--                            <h2 class="post-title">-->
+<!--                                Science has not yet mastered prophecy-->
+<!--                            </h2>-->
+<!--                            <h3 class="post-subtitle">-->
+<!--                                We predict too much for the next year and yet far too little for the next ten.-->
+<!--                            </h3>-->
+<!--                        </a>-->
+<!--                        <p class="post-meta">Posted by-->
+<!--                            <a href="#">Start Bootstrap</a>-->
+<!--                            on August 24, 2019</p>-->
+<!--                    </div>-->
+<!--                    <hr>-->
+<!--                    <div class="post-preview">-->
+<!--                        <a href="post.html">-->
+<!--                            <h2 class="post-title">-->
+<!--                                Failure is not an option-->
+<!--                            </h2>-->
+<!--                            <h3 class="post-subtitle">-->
+<!--                                Many say exploration is part of our destiny, but it’s actually our duty to future generations.-->
+<!--                            </h3>-->
+<!--                        </a>-->
+<!--                        <p class="post-meta">Posted by-->
+<!--                            <a href="#">Start Bootstrap</a>-->
+<!--                            on July 8, 2019</p>-->
+<!--                    </div>-->
+<!--                    <hr>-->
                     <!-- Pager -->
-                    <div class="clearfix">
-                        <a class="btn btn-primary float-right" href="#">Older Posts &rarr;</a>
-                    </div>
+<!--                    <div class="clearfix">-->
+<!--                        <a class="btn btn-primary float-right" href="#">Older Posts &rarr;</a>-->
+<!--                    </div>-->
+                    <Pager :info="$page.posts.pageInfo"/>
                 </div>
             </div>
         </div>
     </Layout>
 </template>
 
+<page-query>
+query ($page: Int) {
+    posts: allStrapiPost(perPage: 2, page: $page) @paginate {
+        pageInfo {
+            totalPages
+            currentPage
+        }
+        edges {
+            node {
+                id title content
+                createdBy {
+                    id username
+                }
+                tags {
+                    id
+                    title
+                }
+            }
+        }
+    }
+}
+</page-query>
+
 <script>
+import {Pager} from 'gridsome'
+
 export default {
-  metaInfo: {
-    title: 'Hello, world!'
-  },
-    name: 'HomePage'
+    metaInfo: {
+        title: 'Hello, world!'
+    },
+    name: 'HomePage',
+    components: {
+        Pager
+    }
 }
 </script>
 
